76bb3377586458353cc27ca0516c85928794d4da
emilym <emily.markova@gmail.com>
2025-01-09 15:26:25 -0800

    add classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,197 +12,14 @@ import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 import Person from '@mui/icons-material/Person';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
-
-type Options = {
-  initialActiveIndex: null | number;
-  vertical: boolean;
-  handlers?: {
-    onKeyDown: (
-      event: React.KeyboardEvent<HTMLAnchorElement>,
-      fns: { setActiveIndex: React.Dispatch<React.SetStateAction<number | null>> },
-    ) => void;
-  };
-};
-
-const useRovingIndex = (options?: Options) => {
-  const {
-    initialActiveIndex = 0,
-    vertical = false,
-    handlers = {
-      onKeyDown: () => {},
-    },
-  } = options || {};
-  const [activeIndex, setActiveIndex] = React.useState<number | null>(
-    initialActiveIndex!,
-  );
-  const targetRefs = React.useRef<Array<HTMLAnchorElement>>([]);
-  const targets = targetRefs.current;
-  const focusNext = () => {
-    let newIndex = activeIndex! + 1;
-    if (newIndex >= targets.length) {
-      newIndex = 0;
-    }
-    targets[newIndex]?.focus();
-    setActiveIndex(newIndex);
-  };
-  const focusPrevious = () => {
-    let newIndex = activeIndex! - 1;
-    if (newIndex < 0) {
-      newIndex = targets.length - 1;
-    }
-    targets[newIndex]?.focus();
-    setActiveIndex(newIndex);
-  };
-  const getTargetProps = (index: number) => ({
-    ref: (ref: HTMLAnchorElement) => {
-      if (ref) {
-        targets[index] = ref;
-      }
-    },
-    tabIndex: activeIndex === index ? 0 : -1,
-    onKeyDown: (event: React.KeyboardEvent<HTMLAnchorElement>) => {
-      if (Number.isInteger(activeIndex)) {
-        if (event.key === (vertical ? 'ArrowDown' : 'ArrowRight')) {
-          focusNext();
-        }
-        if (event.key === (vertical ? 'ArrowUp' : 'ArrowLeft')) {
-          focusPrevious();
-        }
-        handlers.onKeyDown?.(event, { setActiveIndex });
-      }
-    },
-    onClick: () => {
-      setActiveIndex(index);
-    },
-  });
-  return {
-    activeIndex,
-    setActiveIndex,
-    targets,
-    getTargetProps,
-    focusNext,
-    focusPrevious,
-  };
-};
-
-type AboutMenuProps = {
-  focusNext: () => void;
-  focusPrevious: () => void;
-  onMouseEnter?: (event?: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-  onKeyDown?: (event: React.KeyboardEvent<HTMLAnchorElement>) => void;
-};
-
-const AboutMenu = React.forwardRef(
-  (
-    { focusNext, focusPrevious, ...props }: AboutMenuProps,
-    ref: React.ForwardedRef<HTMLAnchorElement>,
-  ) => {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLAnchorElement | null>(null);
-    const { targets, setActiveIndex, getTargetProps } = useRovingIndex({
-      initialActiveIndex: null,
-      vertical: true,
-      handlers: {
-        onKeyDown: (event, fns) => {
-          if (event.key.match(/(ArrowDown|ArrowUp|ArrowLeft|ArrowRight)/)) {
-            event.preventDefault();
-          }
-          if (event.key === 'Tab') {
-            setAnchorEl(null);
-            fns.setActiveIndex(null);
-          }
-          if (event.key === 'ArrowLeft') {
-            setAnchorEl(null);
-            focusPrevious();
-          }
-          if (event.key === 'ArrowRight') {
-            setAnchorEl(null);
-            focusNext();
-          }
-        },
-      },
-    });
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'about-popper' : undefined;
-    return (
-      <ClickAwayListener onClickAway={() => setAnchorEl(null)}>
-        <div onMouseLeave={() => setAnchorEl(null)}>
-          <ListItemButton
-            aria-haspopup
-            aria-expanded={open ? 'true' : 'false'}
-            ref={ref}
-            {...props}
-            role="menuitem"
-            onKeyDown={(event) => {
-              props.onKeyDown?.(event);
-              if (event.key.match(/(ArrowLeft|ArrowRight|Tab)/)) {
-                setAnchorEl(null);
-              }
-              if (event.key === 'ArrowDown') {
-                event.preventDefault();
-                targets[0]?.focus();
-                setActiveIndex(0);
-              }
-            }}
-            onFocus={(event) => setAnchorEl(event.currentTarget)}
-            onMouseEnter={(event) => {
-              props.onMouseEnter?.(event);
-              setAnchorEl(event.currentTarget);
-            }}
-            sx={[open && ((theme) => theme.variants.plainHover.neutral)]}
-          >
-            <Person /> <KeyboardArrowDown />
-          </ListItemButton>
-          <Popper id={id} open={open} anchorEl={anchorEl} disablePortal keepMounted>
-            <List
-              role="menu"
-              aria-label="About"
-              variant="outlined"
-              sx={{
-                my: 2,
-                boxShadow: 'md',
-                borderRadius: 'sm',
-                '--List-radius': '8px',
-                '--List-padding': '4px',
-                '--ListDivider-gap': '4px',
-                '--ListItemDecorator-size': '32px',
-              }}
-            >
-              <ListItem role="none">
-                <ListItemButton role="menuitem" {...getTargetProps(0)}>
-                  <ListItemDecorator>
-									<Person />
-                  </ListItemDecorator>
-                  Account
-                </ListItemButton>
-              </ListItem>
-              <ListItem role="none">
-                <ListItemButton role="menuitem" {...getTargetProps(1)}>
-                  <ListItemDecorator>
-									<InfoOutlinedIcon />
-                  </ListItemDecorator>
-                  Instructions
-                </ListItemButton>
-							</ListItem>
-							<ListItem role="none">
-                <ListItemButton role="menuitem" {...getTargetProps(2)}>
-                  <ListItemDecorator>
-									<ExitToAppOutlinedIcon />
-                  </ListItemDecorator>
-                  Log Out
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Popper>
-        </div>
-      </ClickAwayListener>
-    );
-  },
-);
+import IconButton from '@mui/joy/IconButton';
+import Menu from '@mui/joy/Menu';
+import Dropdown from '@mui/joy/Dropdown';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import MoreVert from '@mui/icons-material/MoreVert';
 
 export default function NavBar() {
-  const { targets, getTargetProps, setActiveIndex, focusNext, focusPrevious } =
-    useRovingIndex();
   return (
     <Box sx={{ minHeight: "20px" }}>
       <List
@@ -218,7 +35,6 @@ export default function NavBar() {
         <ListItem role="none">
           <ListItemButton
             role="menuitem"
-            {...getTargetProps(0)}
             component="a"
             href="#navigation-menu"
           >
@@ -226,15 +42,19 @@ export default function NavBar() {
           </ListItemButton>
         </ListItem>
         <ListItem role="none" sx={{ marginInlineStart: 'auto' }} >
-          <AboutMenu
-            onMouseEnter={() => {
-              setActiveIndex(1);
-              targets[1].focus();
-            }}
-            focusNext={focusNext}
-            focusPrevious={focusPrevious}
-            {...getTargetProps(1)}
-          />
+        <Dropdown>
+      <MenuButton
+        slots={{ root: Person }}
+        slotProps={{ root: { variant: 'outlined', color: 'neutral' } }}
+      >
+        <MoreVert />
+      </MenuButton>
+      <Menu>
+        <MenuItem><Person /> Profile</MenuItem>
+        <MenuItem><InfoOutlinedIcon /> Instructions</MenuItem>
+        <MenuItem><ExitToAppOutlinedIcon /> Logout</MenuItem>
+      </Menu>
+    </Dropdown>
         </ListItem>
       </List>
     </Box>
--- src/components/SeatingChart.tsx
+++ src/components/SeatingChart.tsx
@@ -28,9 +28,13 @@ const marks = [
     value: 900,
     label: "900",
   },
+  {
+    value: 1200,
+    label: "1200",
+  },
 ];
 
-type Item = {
+class Item {
   id: string;
   shape: string;
   name: string;
@@ -38,18 +42,92 @@ type Item = {
   yValue: number;
   widthValue: number;
   heightValue: number;
-};
 
-type Desk = {
-  id: string;
-  shape: string;
-  name: string;
-  specialAccommodations: string[];
-  xValue: number;
-  yValue: number;
-  widthValue: number;
-  heightValue: number;
-};
+  constructor(id: string,
+    shape: string,
+    name: string,
+    xValue: number,
+    yValue: number,
+    widthValue: number,
+    heightValue: number) {
+    this.id = id;
+    this.shape = shape;
+    this.name = name;
+    this.xValue = xValue;
+    this.yValue = yValue;
+    this.widthValue = widthValue;
+    this.heightValue = heightValue;
+  }
+
+  getShape() {
+    return this.shape;
+  }
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
+  }
+  getXValue() {
+    return this.xValue;
+  }
+  getYValue() {
+    return this.yValue;
+  }
+  getWidthValue() {
+    return this.widthValue;
+  }
+  getHeightValue() {
+    return this.heightValue;
+  }
+
+  setShape(newShape: string) {
+    this.shape = newShape;
+  }
+  setId() {
+    return this.id;
+  }
+  setName(newName: string) {
+    this.name = newName;
+  }
+  setXValue(newXValue: number) {
+    this.xValue = newXValue;
+  }
+  setYValue(newYValue: number) {
+    this.yValue = newYValue;
+  }
+  setWidthValue(newWidthValue: number) {
+    this.widthValue = newWidthValue;
+  }
+  setHeightValue(newHeightValue: number) {
+    this.heightValue = newHeightValue;
+  }
+}
+
+class Desk extends Item {
+  accomidations: string[];
+  constructor(id: string,
+    shape: string,
+    name: string,
+    xValue: number,
+    yValue: number,
+    widthValue: number,
+    heightValue: number,
+    accomidations: string[]) {
+    super(id, shape, name, xValue, yValue, widthValue, heightValue);
+    this.accomidations = accomidations;
+  }
+
+  setAccomidations(newAccomidations: string[]) {
+    this.accomidations = newAccomidations;
+  }
+
+  getAccomidations() {
+    return this.accomidations;
+  }
+  
+}
+
 
 function valueText(value: number) {
   return `${value}`;
@@ -70,6 +148,8 @@ export default function SeatingChart() {
   const [objShapeToAdd, setObjShapeToAdd] = useState("rectangle");
   const [objLabel, setObjLabel] = useState("Label");
   const [objAccomidations, setObjAccomidations] = useState([]);
+  const [currItem, setCurrItem] = useState();
+  const [currDesk, setCurrDesk] = useState();
 
 
   const updateHeight = (e: any) => {
@@ -88,7 +168,7 @@ export default function SeatingChart() {
   const updateObjShape = (e: any) => {
     setObjShapeToAdd(e.target.value);
   };
-  
+
   const updateObjAccomidations = (e: any) => {
     setObjAccomidations(e.target.value);
   };
@@ -102,39 +182,34 @@ export default function SeatingChart() {
     setObjY(parseInt(e.target.value, 10) || 50);
   };
 
+  // const updateCurrentThingToChange = (item: any) => {
+  //   // setItemToChange
+  //   if (item is Item) {
+
+  //   }
+  // };
+
   const addItem = () => {
-    let item: Item = {
-      id: uuidv4(),
-      shape: objShapeToAdd,
-      name: objLabel,
-      xValue: objX,
-      yValue: objY,
-      widthValue: objWidth,
-      heightValue: objHeight,
-    };
-    setItems([...items, item]);
+    setItems([...items, new Item(uuidv4(),
+      objShapeToAdd,
+      objLabel,
+      objX,
+      objY,
+      objWidth,
+      objHeight,)]);
   };
 
   const addDesk = () => {
     const accomidationElement = document.getElementById('specialAccommodations');
-    let accomidations:string[];
+    let accomidations: string[];
     if (accomidationElement) {
       accomidations = Array.from(accomidationElement.querySelectorAll('[aria-selected="true"]')).map((option) => option.getAttribute('data-value')).filter((value): value is string => value !== null);
     } else {
       accomidations = [];
     }
+
     
-    let desk: Desk = {
-      id: uuidv4(),
-      shape: objShapeToAdd,
-      name: objLabel,
-      specialAccommodations: objAccomidations,
-      xValue: objX,
-      yValue: objY,
-      widthValue: objWidth,
-      heightValue: objHeight,
-    };
-    setDesks([...desks, desk]);
+    setDesks([...desks, new Desk(uuidv4(), objShapeToAdd, objLabel, objX, objY, objWidth, objHeight, objAccomidations)]);
   };
 
   const clearItems = () => {
@@ -164,10 +239,10 @@ export default function SeatingChart() {
     event: React.SyntheticEvent | null,
     newValue: string | null
   ) => {
-    if (newValue != null) { 
+    if (newValue != null) {
       setObjToAdd(newValue);
 
-    } 
+    }
   };
 
   return (
@@ -239,107 +314,107 @@ export default function SeatingChart() {
           onChange={updateHeight}
         />
 
-        <Box sx={{ minWidth: 240}}>
-      <Box
-        sx={{
-          mb: 2,
-          display: 'flex',
-          justifyContent: 'space-between',
+        <Box sx={{ minWidth: 240 }}>
+          <Box
+            sx={{
+              mb: 2,
+              display: 'flex',
+              justifyContent: 'space-between',
               alignItems: 'center',
-          width:"100%"
-        }}
-      >
-      </Box>
-      <RadioGroup
-        aria-labelledby="example-payment-channel-label"
-        overlay
-        name="example-payment-channel"
+              width: "100%"
+            }}
+          >
+          </Box>
+          <RadioGroup
+            aria-labelledby="example-payment-channel-label"
+            overlay
+            name="example-payment-channel"
             defaultValue="placeholder"
-            sx={{alignItems: 'center', justifyContent:"center"}}
-      >
-        <List
-          component="div"
-          variant="outlined"
-          orientation={"horizontal"}
-          sx={{border:"none"}}
-        >
-          {['placeholder', 'desk'].map((value, index) => (
-            <React.Fragment key={value}>
-              <ListItem>
-                <Radio id={value} value={value} label={value} onClick={()=>{setObjToAdd(value)}} />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
-      </RadioGroup>
-    </Box>
+            sx={{ alignItems: 'center', justifyContent: "center" }}
+          >
+            <List
+              component="div"
+              variant="outlined"
+              orientation={"horizontal"}
+              sx={{ border: "none" }}
+            >
+              {['placeholder', 'desk'].map((value, index) => (
+                <React.Fragment key={value}>
+                  <ListItem>
+                    <Radio id={value} value={value} label={value} onClick={() => { setObjToAdd(value) }} />
+                  </ListItem>
+                </React.Fragment>
+              ))}
+            </List>
+          </RadioGroup>
+        </Box>
         <Box
-        sx = {{backgroundColor:"green"}}>
+          sx={{ backgroundColor: "green" }}>
           <Box
             sx={{
               margin: "5px",
               display: "flex",
-              flexDirection:"column",
+              flexDirection: "column",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
             <RadioGroup
-        aria-labelledby="example-payment-channel-label"
-        overlay
-        name="example-payment-channel"
-            defaultValue="placeholder"
-            sx={{alignItems: 'center', justifyContent:"center"}}
-      >
-        <List
-          component="div"
-          variant="outlined"
-          orientation={"horizontal"}
-          sx={{border:"none"}}
-        >
-          {['circle', 'rectangle'].map((value, index) => (
-            <React.Fragment key={value}>
-              <ListItem>
-                <Radio id={value} value={value} label={value} onClick={()=>{setObjShapeToAdd(value)}} />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
-      </RadioGroup>
+              aria-labelledby="example-payment-channel-label"
+              overlay
+              name="example-payment-channel"
+              defaultValue="placeholder"
+              sx={{ alignItems: 'center', justifyContent: "center" }}
+            >
+              <List
+                component="div"
+                variant="outlined"
+                orientation={"horizontal"}
+                sx={{ border: "none" }}
+              >
+                {['circle', 'rectangle'].map((value, index) => (
+                  <React.Fragment key={value}>
+                    <ListItem>
+                      <Radio id={value} value={value} label={value} onClick={() => { setObjShapeToAdd(value) }} />
+                    </ListItem>
+                  </React.Fragment>
+                ))}
+              </List>
+            </RadioGroup>
             {objToAdd === "desk"
               ? <Box sx={{ display: "inline-block", margin: "10px" }}>
                 <Typography sx={{ textSize: "9px", marginBottom: "10px" }}>
-              Special Accomidations :
-            </Typography>
+                  Special Accomidations :
+                </Typography>
                 <Select
                   multiple
-                  sx={{maxWidth:"100%"}}
-        defaultValue={['plan504', 'fs']}
-        renderValue={(selected) => (
-          <Box sx={{ display: 'flex', gap: '0.25rem' , margin:"0px"}}>
-            {selected.map((selectedOption) => (
-              <Chip variant="soft" color="primary">
-                {selectedOption.label}
-              </Chip>
-            ))}
-          </Box>
-        )}
-        slotProps={{
-          listbox: {
-            sx: {
-              minWidth: '150px',
-              maxWidth:"100%"
-            },
-          },
-        }}
+                  sx={{ maxWidth: "100%" }}
+                  defaultValue={['plan504', 'fs']}
+                  renderValue={(selected) => (
+                    <Box sx={{ display: 'flex', gap: '0.25rem', margin: "0px" }}>
+                      {selected.map((selectedOption) => (
+                        <Chip variant="soft" color="primary">
+                          {selectedOption.label}
+                        </Chip>
+                      ))}
+                    </Box>
+                  )}
+                  slotProps={{
+                    listbox: {
+                      sx: {
+                        minWidth: '150px',
+                        maxWidth: "100%"
+                      },
+                    },
+                  }}
                   id="specialAccomidations"
-      >
-        <Option value="plan504">504 Plan</Option>
-        <Option value="esl">English as a Second Language</Option>
-        <Option value="fs">Front Seat</Option>
-      </Select></Box>
-        : <Box></Box>
-      }
+                >
+                  <Option value="plan504">504 Plan</Option>
+                  <Option value="esl">English as a Second Language</Option>
+                  <Option value="fs">Front Seat</Option>
+                </Select></Box>
+              : <Box></Box>
+            }
             <Typography sx={{ textSize: "9px", marginBottom: "10px" }}>
               Label :
             </Typography>
@@ -441,79 +516,97 @@ export default function SeatingChart() {
             </Box>
           </Stack>
         </Box>
-        <Box sx={{ display: "flex", flexDirection: "column",  alignItems:"center" }}>
-        <Button variant="solid" onClick={addObj} sx={{width:"50px"}}>
-          Add
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Button variant="solid" onClick={addObj} sx={{ width: "50px" }}>
+            Add
           </Button>
-          <Box sx={{display:"flex", justifyContent:"space-between", margin:"15px", width:"100%"}}>
-        <Button variant="solid" onClick={clearItems} sx={{width:"175px", marginLeft:"15px"}}>
-          Clear Placeholders
-        </Button>
-        <Button variant="solid" onClick={clearDesks} sx={{width:"175px", marginRight:"15px"}}>
-          Clear Desks
+          <Box sx={{ display: "flex", justifyContent: "space-between", margin: "15px", width: "100%" }}>
+            <Button variant="solid" onClick={clearItems} sx={{ width: "175px", marginLeft: "15px" }}>
+              Clear Placeholders
             </Button>
-            </Box>
+            <Button variant="solid" onClick={clearDesks} sx={{ width: "175px", marginRight: "15px" }}>
+              Clear Desks
+            </Button>
+          </Box>
         </Box>
       </Box>
       {/* .slice().reverse() */}
       {desks.map((element: Desk, indexCurr: number) => {
         return (
           <Rnd
-          key={`desk-${element.id}`}
+            key={`desk-${element.id}`}
             bounds="parent"
-            style={{  display: "flex",
+            style={{
+              display: "flex",
               alignItems: "center",
               justifyContent: "center",
               border: "solid 1px #ddd",
-              background: "#f0f0f0", borderRadius: element.shape === 'rectangle' ? '0px' : '200px'}}
+              background: "#f0f0f0", borderRadius: element.shape === 'rectangle' ? '0px' : '200px'
+            }}
             size={{ width: element.widthValue, height: element.heightValue }}
-    position={{ x: element.xValue, y: element.yValue }}
-            onDragStop={(e, d) => {  setDesks(desks.map((val: Desk, index2: number) => {
-              if (val.id === element.id) {
-                return { ...val, xValue: d.x, yValue: d.y };
-              } 
-              return val;
-          })) }}
+            position={{ x: element.xValue, y: element.yValue }}
+            onDragStop={(e, d) => {
+              setDesks(desks.map((val: Desk, index2: number) => {
+                if (val.getId() === element.getId()) {
+                  element.setXValue(d.x);
+                  element.setYValue(d.y);
+                  return element;
+                }
+                return val;
+              }))
+            }}
             onResizeStop={(e, direction, ref, delta, position) => {
               setDesks(desks.map((val: Desk, index2: number) => {
-                if (val.id === element.id) {
-                  return { ...val, widthValue: parseInt(ref.style.width, 10), heightValue: parseInt(ref.style.height, 10) };
-                } 
+                if (val.getId() === element.getId()) {
+                  element.setWidthValue(parseInt(ref.style.width, 10));
+                  element.setHeightValue(parseInt(ref.style.height, 10));
+                  return element;
+                }
                 return val;
-            })) }}
+              }))
+            }}
           >
-            <Typography sx={{fontSize:"12px", color:"black"}}>{element.name}</Typography>
+            <Typography sx={{ fontSize: "12px", color: "black" }}>{element.name}</Typography>
           </Rnd>
         );
       })}
       {/* .slice().reverse() */}
-       {items.map((element: Item, indexCurr: number) => {
+      {items.map((element: Item, indexCurr: number) => {
         return (
           <Rnd
-          key={`desk-${element.id}`}
-          style={{  display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            border: "solid 1px #ddd",
-            background: "#f0f0f0", borderRadius: element.shape === 'rectangle' ? '0px' : '200px'}}
+            key={`desk-${element.id}`}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "solid 1px #ddd",
+              background: "#f0f0f0", borderRadius: element.shape === 'rectangle' ? '0px' : '200px'
+            }}
             bounds="parent"
             size={{ width: element.widthValue, height: element.heightValue }}
-    position={{ x: element.xValue, y: element.yValue }}
-            onDragStop={(e, d) => {  setItems(items.map((val: Item, index2: number) => {
-              if (val.id === element.id) {
-                return { ...val, xValue: d.x, yValue: d.y };
-            }
-            return val;
-          })) }}
+            position={{ x: element.xValue, y: element.yValue }}
+            onDragStop={(e, d) => {
+              setItems(items.map((val: Item, index2: number) => {
+                if (val.getId() === element.getId()) {
+                  element.setXValue(d.x);
+                  element.setYValue(d.y);
+                  return element;
+                }
+                return val;
+              }))
+            }}
             onResizeStop={(e, direction, ref, delta, position) => {
               setItems(items.map((val: Item, index2: number) => {
-                if (val.id === element.id) {
-                  return { ...val, widthValue: parseInt(ref.style.width, 10), heightValue: parseInt(ref.style.height, 10) };
-                } 
+                if (val.getId() === element.getId()) {
+                  element.setWidthValue(parseInt(ref.style.width, 10));
+                  element.setHeightValue(parseInt(ref.style.height, 10));
+                  return element;
+                }
                 return val;
-            })) }}
+              }))
+            }}
           >
-            <Typography sx={{fontSize:"12px", color:"black"}}>{element.name}</Typography>
+            <Typography sx={{ fontSize: "12px", color: "black" }}>{element.name}</Typography>
           </Rnd>
         );
       })}
