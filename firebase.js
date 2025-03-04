f66bace0b9d4de8ccdf2ad53877bb1d1c4d7a61c
emilym <emily.markova@gmail.com>
2025-03-04 15:37:50 -0800

    add firebase

--- firebase.js
+++ firebase.js
@@ -8,14 +8,15 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6VTvnUyoT4CVLV-oBOMnW7tXB53NSpMY",
-  authDomain: "seating-chart-1a1a1.firebaseapp.com",
-  projectId: "seating-chart-1a1a1",
-  storageBucket: "seating-chart-1a1a1.firebasestorage.app",
-  messagingSenderId: "471468712833",
-  appId: "1:471468712833:web:ba245385fef54295bcbe80",
-  measurementId: "G-LNV1L4V2KT"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
